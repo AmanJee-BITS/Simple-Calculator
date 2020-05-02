c3da3cd365441f3f0f96d2bf2b003268722ffb6d
Amanjee <amangs896@gmail.com>
2020-05-03 02:37:56 +0530

    Final version

--- calc.py
+++ calc.py
@@ -3,14 +3,14 @@ from Tkinter import *
 #creating frame for calculator
 
 def iCalc(source, side):
-    storeObj = Frame(source, borderwidth = 4, bd = 4, bg = "powder blue")
+    storeObj = Frame(source, borderwidth = 4, bd = 4, bg = "orange")
     storeObj.pack(side=side, expand = YES, fill = BOTH)
     return storeObj
 
 #creating Button
 
-def button(source, side,text, command = NONE):
-    storeObj = Button(source, text = text, command = command)
+def button(source, side, text, command = NONE):
+    storeObj = Button(source, text=text, command=command)
     storeObj.pack(side=side, expand = YES, fill = BOTH)
     return  storeObj
 
@@ -22,13 +22,39 @@ class app(Frame):
         self.master.title('Calculator')
        
         display = StringVar()
-        Entry(self, relief=RIDGE, textvariable=display, justify='right', bd=30, bg="powder blue").pack(side=TOP, expand=YES, fill=BOTH)
+        Entry(self, relief=RIDGE, textvariable=display, justify='right', bd=25, bg="powder blue").pack(side=TOP, expand=YES, fill=BOTH)
 
         for clearButton in (["C"]):
                 erase = iCalc(self, TOP)
                 for ichar in clearButton:
                     button(erase, LEFT, ichar, lambda
                         storeObj=display, q=ichar: storeObj.set(''))
+        
+        for numButton in ("789/", "456*", "123-", "0.+"):
+            FunctionNum = iCalc(self, TOP)
+            for iEquals in numButton:
+                button(FunctionNum, LEFT, iEquals, lambda
+                        storeObj=display, q=iEquals: storeObj
+                            .set(storeObj.get() + q))
+
+        EqualButton = iCalc(self, TOP)
+        for iEquals in "=":
+            if iEquals == '=':
+                btniEquals = button(EqualButton, LEFT, iEquals)
+                btniEquals.bind('<ButtonRelease-1>', lambda e,s=self,
+                                storeObj=display: s.calc(storeObj), '+')
+
+
+            else:
+                btniEquals = button(EqualButton, LEFT, iEquals,
+                                    lambda storeObj=display, s=' %s ' % iEquals: storeObj.set
+                                    (storeObj.get() + s))
+
+    def calc(self, display):
+            try:
+                display.set(eval(display.get()))
+            except:
+                display.set("ERROR")
 
 # Start the GUI
 
